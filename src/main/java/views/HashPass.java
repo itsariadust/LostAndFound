3c20148b7fc7cbf17ccc446ab3f41af531206021
itsariadust <jakobaindreas11@gmail.com>
2025-05-19 00:31:34 +0800

    Deleted test class for bcrypt. Oops.

--- src/main/java/views/HashPass.java
+++
@@ -1,9 +0,0 @@
-package views;
-
-import org.mindrot.jbcrypt.BCrypt;
-
-public class HashPass {
-    public static void main(String[] args) {
-        System.out.println(BCrypt.hashpw("password", BCrypt.gensalt()));
-    }
-}
